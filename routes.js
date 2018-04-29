662754a240ab213e479262b1e94740c51a60288a
Iris Baron <32202824+irisbaron@users.noreply.github.com>
2018-04-29 09:19:38 -0400

    Rename vars

--- routes.js
+++ routes.js
@@ -1,29 +1,27 @@
 module.exports = function(app) {
 
-    var vsam = require('./controller.js');
+    var vsam_controller = require('./controller.js');
 
     // Create a new VSAM file
-    app.post('/vsam/:path', vsam.createFile);
+    app.post('/vsam/:path', vsam_controller.createFile);
 
     // Delete a VSAM file
-    app.delete('/vsam/:path', vsam.deleteFile);
+    app.delete('/vsam/:path', vsam_controller.deleteFile);
 
     // Create a new VSAM record
-    app.post('/vsam/record/:path&:key&:name&:gender', vsam.createRecord);
+    app.post('/vsam/record/:path&:key&:name&:gender', vsam_controller.createRecord);
 
     // Read a single VSAM record
-    app.get('/vsam/record/:path', vsam.readRecord);
+    app.get('/vsam/record/:path', vsam_controller.readRecord);
 
-    // Update a VSAM record 
-    app.put('/vsam/record/:path&:key&:name&:gender', vsam.updateRecord);
+    // Update a VSAM record
+    app.put('/vsam/record/:path&:key&:name&:gender', vsam_controller.updateRecord);
 
     // Delete a VSAM record
-   app.delete('/vsam/record/:path&:key', vsam.deleteRecord);
+   app.delete('/vsam/record/:path&:key', vsam_controller.deleteRecord);
 
    // Read all VSAM records
-     app.get('/vsam/records/:path', vsam.readAllRecords);
+     app.get('/vsam/records/:path', vsam_controller.readAllRecords);
 
 
 }
-
-
