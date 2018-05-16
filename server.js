eee06394187e1e0b8b5e14b725bf75cd28a13d0e
Iris Baron <32202824+irisbaron@users.noreply.github.com>
2018-05-16 11:22:49 -0400

    Minor edit comments to server.js

--- server.js
+++ server.js
@@ -13,20 +13,19 @@
 */
 'use strict'
 
+// define required modules
 var express = require('express');
-
 
 // create express app
 var app = express();
 console.log("Server is up for VSAM application");
-
 
 // define a simple route
 app.get('/', function(req, res){
     res.json({"message": "Welcome to vsam application."});
 });
 
-// Require Notes routes
+// Require VSAM routes
 require('./routes.js')(app);
 
 // listen for requests
